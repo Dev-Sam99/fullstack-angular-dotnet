db3d4de3cc26b7ec28ea40b3c4c0c2fc518c6eb4
Dev-Sam99 <developers.sam99@gmail.com>
2025-07-09 17:36:54 +0530

    appsetting changes

--- ServerApp/obj/Debug/net9.0/ServerApp.AssemblyInfo.cs
+++ ServerApp/obj/Debug/net9.0/ServerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+369726d209dcec0d85b34118e5e8819fff478a67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fac4a380e6d24223c7d90fe6cf1c9b1c3e0e96b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
